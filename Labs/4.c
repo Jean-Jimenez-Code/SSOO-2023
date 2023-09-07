8c72dc82c1fd5ee16b07184c1e5a04a365716d23
Jean-Code-J <71149487+Jean-Jimenez-Code@users.noreply.github.com>
2023-09-07 11:13:59 -0300

    2

--- Labs/4.c
+++ Labs/4.c
@@ -2,14 +2,28 @@
 
 int main(void){
     // Escribe un algoritmo en C que genere la suma de los primero 100 números impares
-        printf("Ingresa 4 numeros decimales, punto flotante\n");
-    float a = 0, b = 0, c=0, d=0, promedio;
-    scanf("%f", &a);
-    scanf("%f", &b);
-    scanf("%f", &c);
-    scanf("%f", &d);
+        printf("Ingresa 3 lados del triangulo\n");
+    int a = 0, b = 0, c=0, perimetro, semiperimetro;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
 
-    promedio=(a+b+c+d)/4;
-    printf("El Promedio es %f", promedio);
-
+    if (a==b && b==c)
+    {
+        printf("El triangulo es Equilatero");
+    }else if (a==b||c==b||a==c)
+    {
+        if (b!=c || a!=c)
+        {
+            printf("El triangulo es Isosceles");
+        }
+    }else if (a!=b&&b!=c)
+    {
+        printf("El triangulo es Escaleno");
+    }
+    
+    perimetro=a+b+c;
+    semiperimetro=(a+b+c)/2;
+    area=sqrt(s*(s-a)*(s-b)*(s-c))
+    printf("El triangulo es Escaleno");
 }
